8669a32579daa3921836811701b59f6e837cb4d6
Javier Razo <javier.razo1221@gmail.com>
2024-04-27 11:10:40 -0600

    My Initial commit
    	new file:   .dockerignore

---
+++ project.py
@@ -0,0 +1,7 @@
+# This is a simple Python script that greets the user
+
+# Ask for the user's name
+name = input("What is your name? ")
+
+# Print a personalized greeting
+print(f"Hello, {name}! Welcome to the world of Python.")
